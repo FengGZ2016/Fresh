f016ffd6da11855cb5879919d5ce88d87e80a97d
冯国富 <fgz2016@outlook.com>
2017-12-03 22:25:07 +0800

    实现保存密码功能

--- src/web/LoginServlet.java
+++ src/web/LoginServlet.java
@@ -4,6 +4,7 @@ import bean.User;
 import service.UserService;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,8 @@ import java.sql.SQLException;
  * @date 2017/12/3
  */
 public class LoginServlet extends HttpServlet{
+
+    private final String REMEMBER="yes";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -39,6 +42,21 @@ public class LoginServlet extends HttpServlet{
         }
 
         if (user!=null){
+            //获取是否记住密码
+            String remember=req.getParameter("remember");
+            if (REMEMBER.equals(remember)){
+                //记住密码
+                Cookie nameCookie=new Cookie("name",name);
+                Cookie passwordCookie=new Cookie("password",password);
+                nameCookie.setMaxAge(60*10);
+                passwordCookie.setMaxAge(60*10);
+
+                resp.addCookie(nameCookie);
+                resp.addCookie(passwordCookie);
+
+
+            }
+
             //登录成功
             resp.sendRedirect(req.getContextPath()+"/category-list.jsp");
 
